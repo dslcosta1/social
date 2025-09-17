9b204337af3f4e5e5c2f6330a225f36e06df460d
dslcosta1 <dslcosta2016@gmail.com>
2025-09-17 08:27:45 -0300

    Add hot reload on the project

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,22 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+
+	"github.com/dslcosta1/social/internal/env"
+)
 
 func main() {
-	cfg := config {
-		addr: ":8080",
+	cfg := config{
+		addr: env.GetString("ADRR", ":8080"),
 	}
 
 	app := &application{
 		config: cfg,
 	}
+
+	os.LookupEnv("PATH")
 
 	mux := app.mount()
 
