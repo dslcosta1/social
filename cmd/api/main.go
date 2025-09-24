d57e68ab09bb18d2c3daa4833334d5baf63860de
dslcosta1 <dslcosta2016@gmail.com>
2025-09-24 08:27:49 -0300

    Add postergress docker image

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/dslcosta1/social/internal/db"
 	"github.com/dslcosta1/social/internal/env"
 	"github.com/dslcosta1/social/internal/env/store"
 )
@@ -11,12 +12,33 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/socialnetwork?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
-	store := store.NewStorage(nil)
+
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Println("database connection pool estabilished")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
-		store: store,
+		store:  store,
 	}
 
 	os.LookupEnv("PATH")
